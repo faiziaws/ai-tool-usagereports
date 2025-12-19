3edc3e689f10ad2865a1c6f946ae2b95c0d2078f
faiziaws <83235606+faiziaws@users.noreply.github.com>
2025-12-19 15:44:28 +0530

    Update app.py
    update DB details

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ st.title("AI Tool Usage Reports – India")
 st.subheader("Location-wise AI Tool Usage")
 
 data = {
-    "State": ["Delhi", "Maharashtra"],
-    "City": ["New Delhi", "Mumbai"],
-    "AI Tool": ["ChatGPT", "Copilot"],
-    "Usage Count": [5, 3]
+    "State": ["Delhi", "Maharashtra","Jharkhand","Bihar","Orisa",],
+    "City": ["New Delhi", "Mumbai","Ranchi","Jamshedpur","Dhanbad","Bhuwaneshwar","cuttak","Patna",],
+    "AI Tool": ["ChatGPT", "Copilot","Gemini","purplexcity"],
+    "Usage Count": [13,11,9,7,5, 3,2,]
 }
 
 df = pd.DataFrame(data)
 st.dataframe(df)
 
 st.success("URL: /aitool-usagereports")
+
